af5a275ac75fc9bd0a6e498f86f8b233d391f958
tokiyanagi <109673296+tokiyanagi@users.noreply.github.com>
2022-09-21 14:48:14 +0800

    Add files via upload

---
+++ 求和/求和/求和.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, sum;//程序的声明部分，定义 a b sum为整型变量
+	a = 123;b = 456;//赋值语句
+	sum = a + b;/*计算
+				并把结果存放到sum中*/
+	printf("合计 %d\n", sum);//输出结果
+	return 0;
+}
